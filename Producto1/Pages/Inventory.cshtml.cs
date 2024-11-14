bce08202ef8ef040abe9162952e7afd8f4630d99
PedroRenatoES <pedrorenatoes@gmail.com>
2024-11-14 12:29:19 -0400

    Pagina Vehicruz

---
+++ Producto1/Pages/Inventory.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Producto1.Pages
+{
+    public class InventoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
